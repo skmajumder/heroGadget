a8985c3d595d80d8fcf7072519b93c64d1f4765e
skmajumder <skmajumder.cse@gmail.com>
2023-04-09 16:35:14 +0600

    Cart products total price

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,16 +3,27 @@ import {useLoaderData} from "react-router-dom";
 import CartItem from "./Cards/CartItem.jsx";
 
 const Cart = () => {
-    const {cartProducts, products} = useLoaderData();
+    const cartProducts = useLoaderData();
+
+    let totalProductsPrice = 0
+    if (cartProducts.length > 0) {
+        for (const cartProduct of cartProducts) {
+            totalProductsPrice += (cartProduct.price * cartProduct.quantity)
+        }
+    }
+
     return (
         <div className="flex min-h-screen items-start justify-center bg-gray-100 text-gray-900">
             <div className="flex flex-col max-w-3xl p-6 space-y-4 sm:p-10 ">
-                <h2 className="text-xl font-semibold">Review Cart Items</h2>
+                <h2 className="text-xl font-semibold">{cartProducts.length ? 'Review Cart Items' : 'Cart is  Empty'}</h2>
                 <ul className="flex flex-col divide-y divide-gray-700">
                     {cartProducts.map((cartProduct) => (
                         <CartItem key={cartProduct.id} product={cartProduct}/>
                     ))}
                 </ul>
+                <div className="space-y-1 text-right"><p>Total amount: <span
+                    className="font-semibold">{totalProductsPrice}$</span></p>
+                    <p className="text-sm text-gray-400">Not including taxes and shipping costs</p></div>
             </div>
         </div>
     );
--- src/loaders/loadingCartProductsData.js
+++ src/loaders/loadingCartProductsData.js
@@ -1,26 +1,24 @@
 'use strict'
 
 import {getStoredShoppingCart} from "../utilities/fakedb.js";
-import fetchProductData from "./loadingProductsData.js";
-
-// Get shopping cart from local storage
-const cartDataLocal = getStoredShoppingCart();
 
 const getCartProductsData = async () => {
-    if (cartDataLocal) {
-        // Get all the products from DB or JSON file
-        const response = await fetch("products.json");
-        const products = await response.json();
-        const cartProducts = []
-        for (const [productID, productQuantity] of Object.entries(cartDataLocal)) {
-            const foundProduct = products.find((p) => p.id === productID);
-            if (foundProduct) {
-                foundProduct.quantity = productQuantity;
-                cartProducts.push(foundProduct)
-            }
+    // Get all the products from DB or JSON file
+    const response = await fetch("products.json");
+    const products = await response.json();
+
+    // Get shopping cart from local storage
+    const cartDataLocal = getStoredShoppingCart();
+
+    const cartProducts = []
+    for (const [productID, productQuantity] of Object.entries(cartDataLocal)) {
+        const foundProduct = products.find((p) => p.id === productID);
+        if (foundProduct) {
+            foundProduct.quantity = productQuantity;
+            cartProducts.push(foundProduct)
         }
-        return {cartProducts, products}
     }
+    return cartProducts
 }
 
 export default getCartProductsData
